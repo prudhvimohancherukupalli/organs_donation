a8724670e946a9d99d00b43a7724ba9b338fab3f
Divya Bathala <S566466@nwmissouri.edu>
2024-03-15 15:49:30 -0500

    added blood group and organs files

---
+++ app/src/main/java/com/example/organs_donation/kidneyList.java
@@ -0,0 +1,14 @@
+package com.example.organs_donation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class kidneyList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kidneylist);
+    }
+}
--- app/src/main/java/com/example/organs_donation/receiverhomepage.java
+++ app/src/main/java/com/example/organs_donation/receiverhomepage.java
@@ -1,14 +1,71 @@
 package com.example.organs_donation;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
 public class receiverhomepage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.receiverhomepage);
+
+        Button heartButton = findViewById(R.id.heartBTN);
+        Button liverButton = findViewById(R.id.liverBTN);
+        Button lungsButton = findViewById(R.id.lungsBTN);
+        Button kidneysButton = findViewById(R.id.kidneysBTN);
+        Button pancreasButton = findViewById(R.id.pancreasBTN);
+        Button smallBowelButton = findViewById(R.id.smallbowelBTN);
+
+        heartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(receiverhomepage.this, heartList.class);
+                startActivity(intent);
+            }
+        });
+
+        liverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(receiverhomepage.this, LiverList.class);
+                startActivity(intent);
+            }
+        });
+
+        lungsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(receiverhomepage.this, lungsList.class);
+                startActivity(intent);
+            }
+        });
+
+        kidneysButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(receiverhomepage.this, kidneyList.class);
+                startActivity(intent);
+            }
+        });
+
+        pancreasButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(receiverhomepage.this, pancreasList.class);
+                startActivity(intent);
+            }
+        });
+
+        smallBowelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(receiverhomepage.this, smallBowelList.class);
+                startActivity(intent);
+            }
+        });
     }
 }
